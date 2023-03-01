c9215da16267b2a7e79ea5de0f3fa4f4b6cdfe2e
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2023-03-01 23:38:48 +0530

    Resolve clang-format violations

--- include/quick-ftxui.hpp
+++ include/quick-ftxui.hpp
@@ -38,7 +38,8 @@ enum block_alignment { VERTICAL, HORIZONTAL };
 
 typedef boost::variant<
     nil, boost::recursive_wrapper<button>, boost::recursive_wrapper<input>,
-    boost::recursive_wrapper<slider>, boost::recursive_wrapper<menu>, boost::recursive_wrapper<expression>>
+    boost::recursive_wrapper<slider>, boost::recursive_wrapper<menu>,
+    boost::recursive_wrapper<expression>>
     node;
 
 struct button {
